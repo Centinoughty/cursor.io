db1d00e29d31e22fa7f47364f8c76507d0cc63a9
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-12-12 00:29:56 +0530

    created an http server

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 require("dotenv").config();
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
